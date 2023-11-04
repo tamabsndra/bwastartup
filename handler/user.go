ca3af96211411cbc3c6d7bf0c3621b1112a26dbd
Narotama Basundara <narotamaaa3@gmail.com>
2023-11-04 10:52:54 +0700

    login endpoint

--- handler/user.go
+++ handler/user.go
@@ -42,3 +42,32 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIresponse("Login failed",http.StatusUnprocessableEntity,"error",errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return 
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+	if err != nil{
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIresponse("Login failed",http.StatusUnprocessableEntity,"error",errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return 
+	}
+
+	formatter := user.FormatterUser(loggedinUser,"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9")
+
+	response := helper.APIresponse("Successfuly loggedin",http.StatusOK,"success",formatter)
+	
+	c.JSON(http.StatusOK, response)
+}
