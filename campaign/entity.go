211a827876b7a9599c0b80461a5312a81c97f148
Narotama Basundara <narotamaaa3@gmail.com>
2023-11-05 09:11:21 +0700

    campaign repository

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,28 @@
 package campaign
+
+import "time"
+
+type Campaign struct {
+	ID               int
+	UserID           int
+	Name             string
+	ShortDescription string
+	Description      string
+	Perks            string
+	BackerCount      string
+	GoalAmount       int
+	CurrentAmount    int
+	Slug             int
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+	CampaignImages	 []CampaignImages
+}
+
+type CampaignImages struct {
+	ID         int
+	CampaignID int
+	FileName   string
+	IsPrimary  int
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
