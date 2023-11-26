3889160417e4c077b5105a2749243b1bfdabaf6b
Narotama Basundara <narotamaaa3@gmail.com>
2023-11-26 21:22:28 +0700

    fetch user endpoint

--- handler/user.go
+++ handler/user.go
@@ -166,3 +166,12 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 
 }
+
+func (h *userHandler) FetchUser(c *gin.Context){
+	currentUser := c.MustGet("currentUser").(user.User)
+	formatter := user.FormatterUser(currentUser,"")
+
+	response := helper.APIresponse("Succesfully fetch user data", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
--- user/formatter.go
+++ user/formatter.go
@@ -1,20 +1,22 @@
 package user
 
 type UserFormatter struct {
-	ID         int			 `json:"id"`       
-	Name       string        `json:"name"`
-	Email      string        `json:"email"`
-	Password   string        `json:"password"`
-	Token	   string 		 `json:"token"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Token    string `json:"token"`
+	ImageURL string `json:"image_url"`
 }
 
 func FormatterUser(user User, token string) UserFormatter {
 	formatter := UserFormatter{
-		ID: user.ID,
-		Name: user.Name,
-		Email: user.Email,
+		ID:       user.ID,
+		Name:     user.Name,
+		Email:    user.Email,
 		Password: user.Password,
-		Token: token,
+		Token:    token,
+		ImageURL: user.Avatar,
 	}
 
 	return formatter
